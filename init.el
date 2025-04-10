3bef291fefec365bd20895f163c3cad697529636
Spencer Mitchell <smitchell556@gmail.com>
2025-04-10 07:45:06 -0400

    Add conda and buffer completion support

--- init.el
+++ init.el
@@ -1,5 +1,6 @@
-;; Full screen mode please.
+;; Full screen mode on start.
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
+
 
 ;; Add MELPA to repo.
 (use-package package
@@ -7,16 +8,43 @@
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
   (package-initialize))
 
+
+;; Completion buffers.
+(use-package ivy
+  :ensure t
+  :config
+  (ivy-mode 1)
+  (setopt ivy-use-virtual-buffers t)
+  (setopt ivy-count-format "(%d/%d) "))
+
+
+;; Conda env support.
+(use-package conda
+  :ensure t
+  :config
+  (conda-env-autoactivate-mode t)
+  (add-hook 'find-file-hook (lambda () (when (bound-and-true-p conda-project-env-path)
+                                         (conda-env-activate-for-buffer))))
+  (custom-set-variables
+   ;; TODO: Search common conda env dirs for one that exists.
+   ;; Replace dir with correct path if different.
+   '(conda-anaconda-home (expand-file-name "~/miniforge3")))
+  (conda-mode-line-setup))
+
+
+;; Magit because of course.
+(use-package magit
+  :ensure t)
+
+
 ;; Better defaults please.
 (use-package better-defaults
   :ensure t)
+
 
 ;; Theme time.
 (use-package modus-themes
   :ensure t
   :config
   (load-theme 'modus-vivendi :no-confirm))
-
-;; Magit because of course.
-(use-package magit
-  :ensure t)
+(put 'dired-find-alternate-file 'disabled nil)
