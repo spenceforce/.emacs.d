e69bd9b93b3209c94ad014c2e8d2b9db8cb5239d
Spencer Mitchell <160069249+SMInsmed@users.noreply.github.com>
2025-05-12 10:09:47 -0400

    Org and python changes (#5)
    * Add org mode
    
    * default to ipython interpreter
    
    * Add todo keywords

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+;;; Emacs init file.
+;;; Any custom or host specific configuration should be placed in `~/.emacs.d/site-lisp/default.el`.
+
+
 ;;; Add MELPA to repo.
 (use-package package
   :config
@@ -35,6 +39,14 @@
   (conda-mode-line-setup))
 
 
+;;; IPython REPL cause it's better than Python.
+(use-package python
+  :ensure t
+  :custom
+  (python-shell-interpreter "ipython")
+  (python-shell-interpreter-args "--simple-prompt -i --InteractiveShell.display_page=True"))
+
+
 ;;; GitHub Copilot
 (use-package copilot-chat
   :ensure t
@@ -60,7 +72,7 @@
   ;; Set API key in `init-extra.el`.
   :ensure t
   :bind
-  (("C-c a" . aidermacs-transient-menu))
+  (("C-c c" . aidermacs-transient-menu))
   :custom
   (aidermacs-use-architect-mode t)
   (aidermacs-show-diff-after-change t))
@@ -83,31 +95,38 @@
   (load-theme 'modus-vivendi :no-confirm))
 
 
+;;; Org mode.
+(use-package org
+  :ensure t
+  :custom
+  (org-log-done t)
+  (org-todo-keywords '((sequence "TODO" "ACTIVE" "|" "DONE")))
+  :config
+  (define-key global-map "\C-cl" 'org-store-link)
+  (define-key global-map "\C-ca" 'org-agenda))
+
+
 ;;; General emacs settings.
 (use-package emacs
   :custom
-  ; Add column and line numbers to bar.
+  ;; Add column and line numbers to bar.
   (column-number-mode t)
   (line-number-mode t)
-  ; Start async shell in a new buffer.
+  ;; Start async shell in a new buffer.
   (async-shell-command-buffer 'new-buffer)
-  ; Human readable file sizes.
+  ;; Human readable file sizes.
   (dired-listing-switches "-alh")
   :config
-  ; Display line numbers in program buffer sidebar.
+  ;; Add `~/.emacs.d/site-lisp` to load path.
+  (add-to-list 'load-path (expand-file-name "site-lisp" user-emacs-directory))
+  ;; Display line numbers in program buffer sidebar.
   (add-hook 'prog-mode-hook 'display-line-numbers-mode)
-  ; Maximize window when using GUI.
+  ;; Maximize window when using GUI.
   (add-to-list 'default-frame-alist '(fullscreen . maximized))
-  ; Automatic paren matching.
+  ;; Automatic paren matching.
   (electric-pair-mode 1)
-  ; Run as a server.
+  ;; Run as a server.
   (server-start))
-
-
-;;; Any host specific config to be run after initialization
-;;; should go in `init-post.el`.
-;;; No error will be thrown if it doesn't exist.
-(load (locate-user-emacs-file "init-post.el") t)
 
 
 (put 'dired-find-alternate-file 'disabled nil)
